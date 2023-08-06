a4b3261d37dbde17bc8a94e9424ee8c27c9728ff
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-08-06 21:50:47 +0000

    movies done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,29 @@
 class MoviesController < ApplicationController
-   def create
+   def update
+    m_id = params.fetch("the_id")
+  
+    matching_records = Movie.where({:id => m_id})
+    the_movie = matching_records.at(0)
+
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    the_movie.save
+
+    redirect_to("/movies/#{the_movie.id}")
+   end
+
+  def create
     m = Movie.new
     m.title = params.fetch("the_title")
     m.year = params.fetch("the_year")
     m.duration = params.fetch("the_duration")
     m.description = params.fetch("the_description")
-    m.image = params.fetch("the_title")
+    m.image = params.fetch("the_image")
     m.director_id = params.fetch("the_director_id")
 
     m.save
